207709618a5272ad875be3253477c14027125311
Oksana Oberemok <Pylypenkok@gmail.com>
2021-08-09 14:25:47 +0300

    coded 3 tasks abot array and .join()

--- mytasks/script.js
+++ mytasks/script.js
@@ -107,3 +107,86 @@
 // }
 // let result = sum / arr.length;
 // console.log(result);
+
+//  Write a JavaScript function to clone an array.
+
+// function copyArray(arr) {
+//   let arrCopy = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     arrCopy.push(arr[i]);
+//   }
+//   return arrCopy;
+// }
+
+// const arr = [1, 2, [4, 0]];
+// const result = copyArray(arr);
+// console.log(result);
+
+// Write a JavaScript function to get the first element of an array.
+// Passing a parameter 'n' will return the first 'n' elements of the array
+
+// function first(arr, amount) {
+//   if (amount <= 0 || arr.length === 0) {
+//     return [];
+//   } else if (amount === undefined) {
+//     return arr[0];
+//   } else {
+//     let newArr = [];
+//     for (let i = 0; i < amount; i++) {
+//       if (i < arr.length) {
+//         newArr.push(arr[i]);
+//       }
+//     }
+//     return newArr;
+//   }
+// }
+
+// console.log(first([7, 9, 0, -2]));
+// console.log(first([], 3));
+// console.log(first([7, 9, 0, -2], 3));
+// console.log(first([7, 9, 0, -2], 6));
+// console.log(first([7, 9, 0, -2], -3));
+
+//4. Write a JavaScript function to get the last element of an array.
+//  Passing a parameter 'n' will return the last 'n' elements of the array.
+
+// function last(arr, n) {
+//   if (n === undefined) {
+//     for (let i = 0; i < arr.length; i++) {
+//       return arr[arr.length - 1];
+//     }
+//   }
+//   if (n > 0) {
+//     let newArr = [];
+//     for (let i = arr.length - 1; i >= 0; i--) {
+//       if (newArr.length < n) {
+//         newArr.push(arr[i]);
+//       }
+//     }
+//     return newArr.reverse();
+//   }
+// }
+
+// // Test Data :
+// console.log(last([7, 9, 0, -2]));
+// console.log(last([7, 9, 0, -2], 3));
+// console.log(last([7, 9, 0, -2, 1], 4));
+// console.log(last([7, 9, 0, -2], 6));
+// console.log(last([7, 9, 0, -2, 1, 3, 4], 6));
+// Expected Output :
+// -2
+// [9, 0, -2]
+// [7, 9, 0, -2]
+
+//5. Write a simple JavaScript program to join all elements of the following array into a string. Go to the editor
+
+// function join(arr) {
+//   return arr.join();//+
+// }
+// const arr = ["Red", "Green", "White", "Black"];
+// console.log(join(arr));
+// Sample array : myColor = ["Red", "Green", "White", "Black"];
+// Expected Output :
+// "Red,Green,White,Black"
+// "Red,Green,White,Black"
+// "Red+Green+White+Black"
