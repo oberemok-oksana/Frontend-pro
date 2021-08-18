1fe7183aa29d39983e692abf44ccd7ca52af32a1
Oksana Oberemok <Pylypenkok@gmail.com>
2021-08-18 14:36:43 +0300

    finished second task

--- js-06/script.js
+++ js-06/script.js
@@ -1,6 +1,4 @@
-// реализовать функции для работы с массивами: map(arr, mapper),
-// some(arr, predicate),
-// every(arr, predicate),
+//в стрелочных функция один параметр в скобках, потому что так сохраняет prettier
 
 function map(arr, func) {
   let newArr = [];
@@ -37,9 +35,68 @@ console.log(every(["one", "two"], (s) => s.length == 3)); //true
 
 // доп. - reduce(arr, predicate,startValue)
 
-// function reduce(arr, f, startValue) {
-//   for (let i = 0; i < arr.length; i++){
+function reduce(arr, f, startValue) {
+  let result = startValue;
+  for (let i = 0; i < arr.length; i++) {
+    result = f(result, arr[i]);
+  }
+  return result;
+}
+console.log(reduce([0, 1, 2, 3, 4], (prev, current) => prev + current, 0)); // -> 10
+console.log(reduce([1, 2, 3, 4], (prev, current) => prev * current, 1)); //24
 
-//   }
+// Задача 2
 
-// }
+const personsInfo = [
+  {
+    name: "Alex",
+    surname: "Petrunko",
+    patronymic: "Ivanovich",
+    phone: "0995551223",
+  },
+  {
+    name: "Olga",
+    surname: "Oganesian",
+    patronymic: "Antonovna",
+    phone: "0931256532",
+  },
+  {
+    name: "Polina",
+    surname: "Pylypenko",
+    patronymic: "Arturovna",
+    phone: "0952363623",
+  },
+  {
+    name: "Vladymyr",
+    surname: "Stognenko",
+    patronymic: "Vladymyrovich",
+    phone: "0978236623",
+  },
+];
+
+function isInPersonsInfo(array) {
+  return array.filter((item) => item.name[0] === item.surname[0]);
+}
+console.log(isInPersonsInfo(personsInfo));
+
+function turnIntoInitials(array) {
+  return array.map(
+    (item) => item.surname + " " + item.name[0] + "." + item.patronymic[0] + "."
+  );
+}
+console.log(turnIntoInitials(personsInfo));
+
+// в) Осуществить поиск контактов по массиву по началу номера телефона (контакт = имя+телефон)
+function showContacts(array, num) {
+  let filteredArray = array.filter((item) => item.phone.startsWith(num));
+  return filteredArray.map((item) => item.name + " " + item.phone);
+}
+
+console.log(showContacts(personsInfo, "093"));
+
+//  г) Проверить наличие телефонного номера среди массива контактов
+
+function findMatch(array, phone) {
+  return array.some((item) => item.phone === phone);
+}
+console.log(findMatch(personsInfo, "0978236623")); //true
