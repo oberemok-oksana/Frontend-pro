92c912fd8903b8dd3fc3309a05ece2f4d90f52f4
Oksana Oberemok <Pylypenkok@gmail.com>
2021-10-07 15:22:16 +0300

    added active class for chosen contact

--- lesson17/scripts/components/contacts.js
+++ lesson17/scripts/components/contacts.js
@@ -13,6 +13,7 @@ class Contacts {
   init() {
     this.containerMyContacts = document.querySelector(".my-contacts-list");
     this.container = document.querySelector(".add-contact-form");
+    this.chosenContactInfo = document.querySelector(".chosen-contact");
     this.nameInput = this.container.querySelector("#contact-name");
     this.typeSelect = this.container.querySelector("#options");
     this.valueInput = this.container.querySelector("#contact");
@@ -33,16 +34,36 @@ class Contacts {
 
     this.containerMyContacts.addEventListener("click", (e) => {
       if (e.target.matches(".my-contacts-list-item")) {
+        let allChosen = document.querySelectorAll(".active-contact");
+        allChosen.forEach((chosen) =>
+          chosen.classList.remove("active-contact")
+        );
+        e.target.classList.add("active-contact");
         const contact = this.contactService.contacts.find((contact) => {
           return contact.id === parseInt(e.target.dataset.id);
         });
+
+        this.chosenContactInfo.innerHTML = "";
+        let contactName = document.createElement("div");
+        contactName.classList.add("my-contacts-list-item");
+        contactName.innerHTML = contact.name;
+        let contactType = document.createElement("div");
+        contactType.classList.add("my-contacts-list-item");
+        contactType.innerHTML = contact.type;
+        let contactValue = document.createElement("div");
+        contactValue.classList.add("my-contacts-list-item");
+        contactValue.innerHTML = contact.value;
+
+        this.chosenContactInfo.append(contactName, contactType, contactValue);
+
+        // this.chosenContactInfo.innerHTML = contact;
       }
     });
   }
 
   showContacts() {
     this.containerMyContacts.innerHTML = "";
-    this.contactService.contacts.map((contact, i) => {
+    this.contactService.contacts.map((contact) => {
       let person = document.createElement("li");
       person.classList.add("my-contacts-list-item");
       person.dataset.id = contact.id;
