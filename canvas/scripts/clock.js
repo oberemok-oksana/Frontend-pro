fcdc824ec3fd919473d495f86e8aed6a0f28dbe9
Oksana Oberemok <Pylypenkok@gmail.com>
2021-10-10 23:25:33 +0300

    Add request animation frame; refactor hand drawing functions

--- canvas/scripts/clock.js
+++ canvas/scripts/clock.js
@@ -1,28 +1,62 @@
 document.addEventListener("DOMContentLoaded", () => {
   let cvs = document.querySelector(".clock");
   let context = cvs.getContext("2d");
+  context.translate(250, 250);
 
-  function drawHourClockHand(hour) {
-    context.rotate((Math.PI / 180) * 30 * hour);
+  function drawClockHand(value, config) {
+    context.rotate((Math.PI / 180) * config.angle * value);
     context.beginPath();
     context.moveTo(0, 0);
-    context.lineTo(0, -40);
-    context.lineWidth = 8;
+    context.lineTo(0, -config.length);
+    context.lineWidth = config.lineWidth;
     context.closePath();
     context.stroke();
-    context.rotate((-Math.PI / 180) * 30 * hour);
+    context.rotate((-Math.PI / 180) * config.angle * value);
+  }
+
+  function drawHourClockHand(hour) {
+    drawClockHand(hour, { angle: 30, length: 40, lineWidth: 8 });
+  }
+
+  function drawMinuteClockHand(minute) {
+    drawClockHand(minute, { angle: 6, length: 75, lineWidth: 5 });
+  }
+
+  function drawSecondClockHand(second) {
+    drawClockHand(second, { angle: 6, length: 85, lineWidth: 3 });
   }
 
   function drawCircle() {
-    context.translate(250, 250);
-    context.beginPath();
     let endAngle = (Math.PI / 180) * 360;
+    context.fillStyle = "white";
+    context.lineWidth = 1;
+    context.arc(0, 0, 100, 0, endAngle);
+    context.fill();
+
+    context.beginPath();
+    context.fillStyle = "black";
     context.arc(0, 0, 100, 0, endAngle);
     context.closePath();
 
     context.stroke();
-    drawHourClockHand(6);
   }
 
-  drawCircle();
+  function drawClock() {
+    let date = new Date();
+    drawCircle();
+    drawHourClockHand(date.getHours());
+    drawMinuteClockHand(date.getMinutes());
+    drawSecondClockHand(date.getSeconds());
+  }
+
+  setInterval(() => drawClock(), 1000);
+
+  // function startAnimation() {
+  //   requestAnimationFrame(() => {
+  //     drawCircle();
+  //     startAnimation();
+  //   });
+  // }
+
+  // startAnimation();
 });
